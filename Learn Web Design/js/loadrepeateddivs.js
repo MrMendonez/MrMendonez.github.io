2132e6908f511a61c13322fcdf766506c04bbc1d
MrMendonez <darrylmendonez@Darryl-Mendonezs-MacBook-Air.local>
2014-10-16 20:25:07 -0400

    Appendix 1
    List of sites with helpful tables of web design codes

---
+++ Learn Web Design/js/loadrepeateddivs.js
@@ -0,0 +1,13 @@
+function loadRepeatedDivs(){
+
+    $("#navigation-bar").load("https://4598ceb4dd3fa10814247ba1accd1c31c1c9f9dd.googledrive.com/host/0B5kZdBK1QTn1amM4clB3Vng0SlU/loadnavigationbar.html");
+
+    $("#contact-pop-up").load("https://4598ceb4dd3fa10814247ba1accd1c31c1c9f9dd.googledrive.com/host/0B5kZdBK1QTn1amM4clB3Vng0SlU/loadcontactpopup.html");
+    
+    $("#google-drive-sidebar").load("#");
+    
+    $("#footnote").load("#");
+
+}
+
+jQuery(document).ready(function() { loadRepeatedDivs(); });
