da0ca5ba224ff3604c320d21c7d6842d4ba7f06d
MrMendonez <darrylmendonez@Darryl-Mendonezs-MacBook-Air.local>
2015-05-04 13:11:43 -0400

    Uploading Portfolio files
    Uploading Portfolio files

---
+++ Darryl_Brian/README.md
@@ -0,0 +1,6 @@
+DarrylMendonez
+==============
+
+A nerdy internet computer hip-hop head
+
+This page will document the many aspects of my life =)
---
+++ Darryl_Brian/js/loadrepeateddivs.js
@@ -0,0 +1,13 @@
+/* Darryl Brian */
+
+function loadRepeatedDivs(){
+
+    $("#navigation-bar").load("https://f5cb3c4667ef6cc580ace48677bc3451ff714fe1.googledrive.com/host/0B5kZdBK1QTn1LVZhZTdBdkFURTA/loadnavigationbar.html");
+
+    $("#contact-pop-up").load("https://f5cb3c4667ef6cc580ace48677bc3451ff714fe1.googledrive.com/host/0B5kZdBK1QTn1LVZhZTdBdkFURTA/loadcontactpopup.html");
+    
+    $("#footer").load("https://f5cb3c4667ef6cc580ace48677bc3451ff714fe1.googledrive.com/host/0B5kZdBK1QTn1LVZhZTdBdkFURTA/loadfooter.html");
+
+}
+
+jQuery(document).ready(function() { loadRepeatedDivs(); });
