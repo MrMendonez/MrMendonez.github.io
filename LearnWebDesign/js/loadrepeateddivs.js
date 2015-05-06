316705b336d0d11b6e44a6adb9cc85603b0b4ecf
MrMendonez <darrylmendonez@Darryl-Mendonezs-MacBook-Air.local>
2015-05-06 12:34:15 -0400

    cleaned up repeateddivs code.
    cleaned up repeateddivs code.

--- LearnWebDesign/js/loadrepeateddivs.js
+++ LearnWebDesign/js/loadrepeateddivs.js
@@ -2,14 +2,10 @@
 
 function loadRepeatedDivs(){
 
-    $("#navigation-bar").load("https://4598ceb4dd3fa10814247ba1accd1c31c1c9f9dd.googledrive.com/host/0B5kZdBK1QTn1amM4clB3Vng0SlU/loadnavigationbar.html");
+    $("#navigation-bar").load("js/loadnavigationbar.html");
 
-    $("#contact-pop-up").load("https://4598ceb4dd3fa10814247ba1accd1c31c1c9f9dd.googledrive.com/host/0B5kZdBK1QTn1amM4clB3Vng0SlU/loadcontactpopup.html");
-    
-    $("#google-drive-sidebar").load("#");
-    
-    $("#footnote").load("#");
+    $("#contact-pop-up").load("js/loadcontactpopup.html");
 
 }
 
-jQuery(document).ready(function() { loadRepeatedDivs(); });
+$(document).ready(function() { loadRepeatedDivs(); });
