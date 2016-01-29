75944be77f4b4cc5c5b7a4ebbb3da2b0e65672e7
Darryl Mendonez <007202@SBSCHOOLS.ORG>
2016-01-29 09:43:24 -0500

    put in my design website into my github page

---
+++ js/loadrepeateddivs.js
@@ -0,0 +1,9 @@
+/* Repeated Divs for 'Design' Site */
+
+function loadrepeateddivs(){
+    
+    $("#navigation-bar").load("js/loadNavigationBar.html");
+    
+}
+
+jQuery(document).ready(function() { loadrepeateddivs(); });
